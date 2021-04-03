33b4dc02d2930d09854f3b316cf8df30e441b1c7
sagarika <sagarikawagh11@gmail.com>
2021-04-03 01:09:25 -0700

    first commit

---
+++ LatestJenkinsProject/src/test/java/com/demo/pack/SignupTest.java
@@ -0,0 +1,12 @@
+package com.demo.pack;
+
+import org.testng.annotations.Test;
+
+public class SignupTest 
+{
+  @Test
+   public void signup()
+   {
+	   System.out.println("Singup Successfully");
+   }
+}
