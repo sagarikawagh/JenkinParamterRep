a674cd3b6279bb0ea78ebb7b78df080dc73eeb7a
sagarika <sagarikawagh11@gmail.com>
2021-04-03 07:29:38 -0700

    Second committe

--- LatestJenkinsProject/src/test/java/com/demo/pack/SignupTest.java
+++ LatestJenkinsProject/src/test/java/com/demo/pack/SignupTest.java
@@ -7,6 +7,11 @@ public class SignupTest
   @Test
    public void signup()
    {
+	    String url = System.getProperty("url");
+		String browser= System.getProperty("browser");
+		
+		System.out.println("username is : " + url);
+		System.out.println("password is : " + browser);
 	   System.out.println("Singup Successfully");
    }
 }
